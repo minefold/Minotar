174cd11f25a13e1eae635946e692db9d9f49189e
Chris Lloyd <christopher.lloyd@gmail.com>
2013-02-02 16:23:38 -0800

    Serve default avatars and cache them.

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
+require 'rack-cache'
 require 'sinatra'
 require 'faraday'
-require 'RMagick'
+require 'rmagick'
 
-get '/' do
-  redirect 'https://minotar.net'
-end
+one_day = 24 * 60 * 60
+
+use Rack::Cache, verbose: true,
+                 default_ttl: one_day,
+                 allow_revalidate: true,
+                 allow_reload: true
 
 get '/helm/:username/:size.png' do |username, size|
   url = "http://s3.amazonaws.com/MinecraftSkins/#{username}.png"
@@ -18,11 +22,20 @@ get '/helm/:username/:size.png' do |username, size|
     head = skin.crop(8, 8, 8, 8)
 
     avatar = head.composite(helm, 0, 0, Magick::AtopCompositeOp)
-
-    content_type :png
-    avatar.sample(size.to_i, size.to_i).to_blob
   else
-    status 404
-    'not found'
+    url = "http://s3.amazonaws.com/MinecraftSkins/char.png"
+    response = Faraday.get(url)
+
+    skin = Magick::ImageList.new
+    skin.from_blob(response.body)
+
+    helm = skin.crop(40, 8, 8, 8)
+    head = skin.crop(8, 8, 8, 8)
+
+    avatar = head.composite(helm, 0, 0, Magick::AtopCompositeOp)
   end
+
+  headers 'Etag' => response.headers['ETag']
+  content_type :png
+  avatar.sample(size.to_i, size.to_i).to_blob
 end
